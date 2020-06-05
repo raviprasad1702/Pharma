de493ed0e6a9b4ec1f20751a105ba970b17f2cdc
Ravi Prasad <58210098+raviprasad1702@users.noreply.github.com>
2020-06-06 00:02:37 +0530

    Add files via upload

---
+++ js/agencies-list.js
@@ -0,0 +1,37 @@
+const searchBox = document.querySelector("#searchBox");
+const autoCompleteList = document.querySelector(".autocomplete-list");
+
+const agencyListSearch = async (searchText) => {
+  const res = await fetch("../a-list.json");
+  const agencyListItems = await res.json();
+
+  let matches = agencyListItems.filter((item) => {
+    const regex = new RegExp(`^${searchText}`, "gi");
+    return item.name.match(regex);
+  });
+
+  if (searchText.length === 0) {
+    matches = [];
+    autoCompleteList.innerHTML = "";
+  }
+
+  // console.log(matches)
+
+  outputList(matches);
+};
+
+const outputList = (matches) => {
+  if (matches.length > 0) {
+    const html = matches
+      .map(
+        (match) => `
+        <div> <a> ${match.name}</a></div>
+    `
+      )
+      .join("");
+
+    autoCompleteList.innerHTML = html;
+  }
+};
+
+searchBox.addEventListener("input", () => agencyListSearch(searchBox.value));
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+let newOrder = document.querySelector('[data-new-order-button]');
+
+
+newOrder.addEventListener('click', e => {
+    window.location.replace('./order.html')
+})
